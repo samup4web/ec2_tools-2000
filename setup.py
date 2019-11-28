69ddec07c40f5f012a90885a99c76da2bb0ca2ea
Samuel Idowu <samuel.idowu@aptiv.com>
2019-11-28 20:39:37 +0100

    Add distribution setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='ec2_tool_2000',
+    version='0.2',
+    author="Samuel O. Idowu",
+    author_email="samup4web@yahoo.com",
+    description="EC2_tool_2000 is a tool to manage EC2 resources",
+    license="GPLv3+",
+    packages=['ec2_tool_2000'],
+    url="https://github.com/samup4web/ec2_tools-2000",
+    install_requires=[
+        'click',
+        'boto3',
+        'botostubs'
+    ],
+    entry_points= {
+        'console_scripts': [
+            'ec2_tool = ec2_tool_2000.ec2_tool_2000:cli'
+        ],
+    }
+)
