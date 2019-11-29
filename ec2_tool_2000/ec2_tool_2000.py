dde3101721e7b6053accade600006fd29206573d
Samuel Idowu <samuel.idowu@aptiv.com>
2019-11-29 23:12:54 +0100

    Add more command options

--- ec2_tool_2000/ec2_tool_2000.py
+++ ec2_tool_2000/ec2_tool_2000.py
@@ -1,28 +1,56 @@
-import boto3, botostubs
+import boto3
+import botostubs
 import botocore
 import click
+import sys
+from datetime import datetime, timezone
 
-session = boto3.Session(profile_name="aws_cli_admin")
-ec2 = session.resource('ec2') #type: botostubs.EC2.Ec2Resource
+
+ec2 = None  # type: botostubs.EC2.Ec2Resource
 
 
 @click.group()
-def cli():
+@click.option('--profile', default=None, help="Specify profile to use for AWS resource management. A 'default' profile is used as default")
+def cli(profile):
     """A custom script to manage EC2 and EBS snapshots"""
+    session = None
+    if profile:
+        try:
+            session = boto3.Session(profile_name=profile)
+            print("Using '{}' profile.".format(profile))
+        except botocore.exceptions.ProfileNotFound as e:
+            sys.exit("Unknown profile '{}'. ".format(profile) + str(e))
+    else:
+        session = boto3.Session(profile_name="default")
+        print("Using 'default' profile.")
 
-"""INSTANCES"""
+    global ec2
+    ec2 = session.resource('ec2')
+    return
+
+
+"""
+ ___ _  _ ___ _____ _   _  _  ___ ___ ___
+|_ _| \| / __|_   _/_\ | \| |/ __| __/ __|
+ | || .` \__ \ | |/ _ \| .` | (__| _|\__ \
+|___|_|\_|___/ |_/_/ \_\_|\_|\___|___|___/
+"""
+
 
 @cli.group('instances')
 def instances():
     """Commands for ec2 instances"""
 
+
 @instances.command('list')
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
-def list_instances(project):
+@click.option('--instance', 'target_instance', default=None, help="Only specified instance")
+def list_instances(project, target_instance):
     "List EC2 instances"
-    instances = filter_instances(project)
+    instances = filter_instances(project, target_instance)
     # print out all instances
-    for i in instances: #type: botostubs.EC2.Ec2Resource.Instance
+    i: botostubs.EC2.Ec2Resource.Instance
+    for i in instances:
         tags = {t['Key']: t['Value'] for t in i.tags or []}
         print(", ".join((
             i.instance_id,
@@ -37,46 +65,85 @@ def list_instances(project):
 
 @instances.command('stop')
 @click.option('--project', default=None, help="Only instances for project:<name>")
-def stop_instances(project):
+@click.option('--force', default=False, is_flag=True, help="Option to force action when the project or instance id is not specified")
+@click.option('--instance', 'target_instance', default=None, help="Only specified instance")
+def stop_instances(project, force, target_instance):
     "Stop EC2 instances"
-    instances = filter_instances(project)
-
-    for i in instances: #type: botostubs.EC2.Ec2Resource
-        print("Stopping {0}...".format(i.instance_id))
-        try:
-            i.stop()
-        except botocore.exceptions.ClientError as e:
-            print("Could not stop {}.".format(i.id) + str(e))
-            continue
+    safeguard_action(project, target_instance, force)
+    instances = filter_instances(project, target_instance)
+    i: botostubs.EC2.Ec2Resource.Instance
+    for i in instances:
+        if not i.state['Name'] == 'stopped':
+            try:
+                print("Stopping {0}...".format(i.instance_id))
+                i.stop()
+            except botocore.exceptions.ClientError as e:
+                print("Could not stop {}.".format(i.id) + str(e))
+                continue
+        else:
+            print("Instance '{}' already in a stopped state".format(i.instance_id))
     return
 
 
 @instances.command('start')
 @click.option('--project', default=None, help="Only instances for project:<name>")
-def start_instances(project):
+@click.option('--force', default=False, is_flag=True, help="Option to force action when the project or instance id is not specified")
+@click.option('--instance', 'target_instance', default=None, help="Only specified instance")
+def start_instances(project, force, target_instance):
     "Start EC2 instances"
-    instances = filter_instances(project)
-    for i in instances:  #type: botostubs.EC2.Ec2Resource
-        print("Starting {0}...".format(i.instance_id))
-        try:
-            i.start()
-        except botocore.exceptions.ClientError as e:
-            print("Could not start {}. ".format(i.id) + str(e))
-            continue
+    safeguard_action(project, target_instance, force)
+    instances = filter_instances(project, target_instance)
+    i: botostubs.EC2.Ec2Resource.Instance
+    for i in instances:
+        if not i.state["Name"] == "running":
+            try:
+                print("Starting {0}...".format(i.instance_id))
+                i.start()
+            except botocore.exceptions.ClientError as e:
+                print("Could not start {}. ".format(i.id) + str(e))
+                continue
+        else:
+            print("Instance '{}' is already in running state".format(i.instance_id))
     return
 
 
-"""VOLUMES"""
+@instances.command('reboot')
+@click.option('--project', default=None, help="Only instances for project:<name>")
+@click.option('--force', default=False, is_flag=True, help="Option to force action when the project or instance id is not specified")
+@click.option('--instance', 'target_instance', default=None, help="Only specified instance")
+def reboot_instances(project, force, target_instance):
+    "Reboot EC2 instances"
+    safeguard_action(project, target_instance, force)
+    instances = filter_instances(project, target_instance)
+    i: botostubs.EC2.Ec2Resource.Instance
+    for i in instances:
+        if i.state['Name'] == 'running':
+            print("Rebooting instance {}".format(i.instance_id))
+            i.reboot()
+        else:
+            print("Reboot is only valid for instances in running state")
+
+
+"""
+__   _____  _   _   _ __  __ ___ ___
+\ \ / / _ \| | | | | |  \/  | __/ __|
+ \ V / (_) | |_| |_| | |\/| | _|\__ \
+  \_/ \___/|____\___/|_|  |_|___|___/
+"""
+
 
 @cli.group('volumes')
 def volumes():
     """ Commands for ebs volumes"""
 
+
 @volumes.command('list')
 @click.option('--project', default=None, help="Only volumes for project (tag Project:<name>)")
-def list_volumes(project):
+@click.option('--instance', 'target_instance', default=None, help="Only specified instance")
+def list_volumes(project, target_instance):
     """Show EBS volumes"""
-    instances = filter_instances(project)
+    instances = filter_instances(project, target_instance)
+    i: botostubs.EC2.Ec2Resource.Instance
     for i in instances:
         v: botostubs.EC2.Ec2Resource.Volume
         for v in i.volumes.all():
@@ -89,18 +156,29 @@ def list_volumes(project):
             )))
     return
 
-"""SNAPSHOTS"""
+
+"""
+ ___ _  _   _   ___  ___ _  _  ___ _____ ___
+/ __| \| | /_\ | _ \/ __| || |/ _ \_   _/ __|
+\__ \ .` |/ _ \|  _/\__ \ __ | (_) || | \__ \
+|___/_|\_/_/ \_\_|  |___/_||_|\___/ |_| |___/
+
+"""
+
 
 @cli.group('snapshots')
 def snapshots():
     """ Commands for ebs snapshots"""
 
+
 @snapshots.command('list')
 @click.option('--project', default=None, help="Only snapshots for project (tag Project:<name>)")
 @click.option('--all', 'list_all', default=False, is_flag=True, help="Option to show all snapshots instead of only the latest")
-def list_snapshots(project, list_all):
+@click.option('--instance', 'target_instance', default=None, help="Only specified instance")
+def list_snapshots(project, list_all, target_instance):
     """Show EBS snapshots"""
-    instances = filter_instances(project)
+    instances = filter_instances(project, target_instance)
+    i: botostubs.EC2.Ec2Resource.Instance
     for i in instances:
         v: botostubs.EC2.Ec2Resource.Volume
         for v in i.volumes.all():
@@ -116,37 +194,61 @@ def list_snapshots(project, list_all):
                     s.start_time.strftime("%c"),
                 )))
 
-                if s.state == 'completed' and not list_all: break
+                if s.state == 'completed' and not list_all:
+                    break
     return
+
 
 @snapshots.command('create')
 @click.option('--project', default=None, help="Only snapshots for project (tag Project:<name>)")
-def create_snapshots(project):
+@click.option('--force', default=False, is_flag=True, help="Option to force action when the project or instance id is not specified")
+@click.option('--instance', 'target_instance', default=None, help="Only specified instance")
+@click.option('--age', default=None, help="Create snapshot of volumes with last snapshot older than 'age' days")
+def create_snapshots(project, force, target_instance, age):
     """Create EBS snapshots"""
-    instances = filter_instances(project)
+    safeguard_action(project, target_instance, force)
+    instances = filter_instances(project, target_instance)
+    running_instances = set()
     i: botostubs.EC2.Ec2Resource.Instance
     for i in instances:
-        print("Stopping EC2 instance: {}".format(i.instance_id))
-        i.stop()
-        i.wait_until_stopped()
         v: botostubs.EC2.Ec2Resource.Volume
         for v in i.volumes.all():
-            if has_pending_snapshot(v):
-                print("Skipping: volume {} has a pending snapshot".format(v.volume_id))
+            time_now = datetime.now(timezone.utc)
+            last_volume_snapshot_time = get_last_snapshot(v)
+            volume_snapshot_age = time_now - last_volume_snapshot_time
+            if volume_snapshot_age.days >= int(age):
+                if has_pending_snapshot(v):
+                    print("Skipping: volume {} has a pending snapshot".format(v.volume_id))
+                else:
+                    try:
+                        if i.state["Name"] == "running":
+                            running_instances.add(i)
+                            print("Stopping EC2 instance: {}".format(i.instance_id))
+                            i.stop()
+                            i.wait_until_stopped()
+                        print("Creating snapshot for instance:{} ...".format(v.volume_id))
+                        v.create_snapshot(Description="Script generated snapshot")
+                    except botocore.exceptions.ClientError as e:
+                        print("Could not create a snapshot for volume: {}. ".format(
+                            v.volume_id) + str(e))
             else:
-                print("Creating snapshot for instance:{} ...".format(v.volume_id))
-                try:
-                    v.create_snapshot(Description="Script generated snapshot")
-                except botocore.exceptions.ClientError as e:
-                    print("Could not create a snapshot for volume: {}. ".format(v.volume_id) + str(e))
+                print("Skipping: A snapshot of volume '{}' has been created in the last '{}' days".format(v.volume_id, age))
+
+    for i in running_instances:
+        print("Re-starting EC2 instance '{}'".format(i.instance_id))
+        i.start()
+
     return
 
 
 @snapshots.command('delete')
 @click.option('--project', default=None, help="Only snapshots for project (tag Project:<name>)")
-def delete_snapshots(project):
+@click.option('--force', default=False, is_flag=True, help="Option to force action when the project or instance id is not specified")
+@click.option('--instance', 'target_instance', default=None, help="Only specified instance")
+def delete_snapshots(project, force, target_instance):
     """Delete EBS snapshots"""
-    instances = filter_instances(project)
+    safeguard_action(project, target_instance, force)
+    instances = filter_instances(project, target_instance)
     i: botostubs.EC2.Ec2Resource.Instance
     for i in instances:
         v: botostubs.EC2.Ec2Resource.Volume
@@ -158,23 +260,53 @@ def delete_snapshots(project):
     return
 
 
+"""
+ _   _ _____ ___ _    ___
+| | | |_   _|_ _| |  / __|
+| |_| | | |  | || |__\__ \
+ \___/  |_| |___|____|___/
 
-#helper-functions
-def filter_instances(project):
+"""
+
+
+def filter_instances(project, target_instance):
     instances = []
-    if project:
-        filters = [{'Name':'tag:Project', 'Values':[project]}]
-        instances = ec2.instances.filter(Filters=filters)
-    else:
+    if not project and not target_instance:
         instances = ec2.instances.all()
+    elif project and target_instance:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+        instances = instances.filter(InstanceIds=[target_instance])
+    else:
+        if target_instance:
+            instances = ec2.instances.filter(InstanceIds=[target_instance])
+        if project:
+            filters = [{'Name': 'tag:Project', 'Values': [project]}]
+            instances = ec2.instances.filter(Filters=filters)
     return instances
+
 
 def has_pending_snapshot(volume):
     snapshots = list(volume.snapshots.all())
-    print(snapshots[0].state)
-    b =  snapshots and snapshots[0].state == 'pending'
-    print(b)
-    return b
-# main
+    return snapshots and snapshots[0].state == 'pending'
+
+def get_last_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    if snapshots:
+        return snapshots[0].start_time
+
+
+def safeguard_action(project, target_instance, force):
+    if not project and not target_instance and not force:
+        sys.exit("Operation denied. Please specify project or use --force flag")
+
+
+"""
+ __  __      _
+|  \/  |__ _(_)_ _
+| |\/| / _` | | ' \
+|_|  |_\__,_|_|_||_|
+"""
+
 if __name__ == "__main__":
     cli()
